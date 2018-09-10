8d7021fb49ebe6c87ef6f4395178d31219ebfc6a
= <u81815466@cbi-bs1.server.lan>
2018-09-10 22:23:36 +0100

    Fixed home blade link

---
+++ resources/views/home.blade.php
@@ -0,0 +1,41 @@
+<html><head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <script type="text/javascript" src="http://netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <link href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="http://pingendo.github.io/pingendo-bootstrap/themes/default/bootstrap.css" rel="stylesheet" type="text/css">
+    <title>ST app</title>
+  </head><body>
+    <div class="cover">
+      <div class="navbar navbar-default">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-ex-collapse">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#"><span>ST App</span><br></a>
+          </div>
+          <div class="collapse navbar-collapse" id="navbar-ex-collapse"></div>
+        </div>
+      </div>
+      <div class="cover-image" style="background-image : url('http://pingendo.github.io/pingendo-bootstrap/assets/blurry/800x600/7.jpg')"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <h1>ST App</h1>
+            <p>Making Student Temp easier to use on mobile</p>
+            <br>
+            <br>
+            <a class="btn btn-lg btn-default" href="{{asset('app-release.apk')}}">Download App</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+  
+
+</body></html>
